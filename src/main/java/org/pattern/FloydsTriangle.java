6c82eac48798378ee77766660406172534b61327
Dhanushchandra <Dhanushbharath9@gmail.com>
2024-02-17 16:36:54 +0530

    Floyd's Triangle

---
+++ src/main/java/org/pattern/FloydsTriangle.java
@@ -0,0 +1,20 @@
+package org.pattern;
+
+public class FloydsTriangle {
+
+    public static void main(String[] args) {
+
+        int rows = 5;
+        int count = 1;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(count + " ");
+                count++;
+            }
+            System.out.println();
+        }
+
+    }
+
+}
